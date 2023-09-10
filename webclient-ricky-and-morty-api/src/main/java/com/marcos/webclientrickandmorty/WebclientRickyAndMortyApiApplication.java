72e1e8b9553daa086b33464b7daec4cd67df39c1
Marcos Brandao <57876319+MarcosBrandao21@users.noreply.github.com>
2023-09-10 00:41:24 -0300

    Criação do projeto

---
+++ webclient-ricky-and-morty-api/src/main/java/com/marcos/webclientrickandmorty/WebclientRickyAndMortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.webclientrickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientRickyAndMortyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientRickyAndMortyApiApplication.class, args);
+	}
+
+}
